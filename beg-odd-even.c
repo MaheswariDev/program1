666d2c575af0f382686edfab6e7b6cc842b441cd
Maheswariraj <maheswarigangaraj@gmail.com>
2016-06-06 11:44:03 +0530

    Update beg-odd-even.c

--- beg-odd-even.c
+++ beg-odd-even.c
@@ -4,8 +4,8 @@ int main()
     int num;
     scanf("%d",&num);
     if(num%2==0)
-       printf("number is odd");
-    else    
        printf("number is even");
+    else    
+       printf("number is odd");
     return 0;
 }
