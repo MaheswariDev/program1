7031197bea47f64672437cb6777fe821344ef0a0
Maheswariraj <maheswarigangaraj@gmail.com>
2016-06-06 12:30:24 +0530

    Create beginer-0dd-even.c

---
+++ beginer-0dd-even.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    scanf("%d",&num);
+    if(num%2==0)
+       printf("number is odd");
+    else    
+       printf("number is even");
+    return 0;
+}
